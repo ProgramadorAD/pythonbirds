7e9efc063d5e67ef7550f2263ef5c93bbebae99c
Andre <andreduartecosta@hotmai.com>
2020-03-27 13:57:02 -0300

    Criados atributos de instânca nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=39):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Andre'
+    print(p.nome)
+    print(p.idade)
